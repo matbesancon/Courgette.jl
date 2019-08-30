0d35d3859bff91cbce0c0d08880208736a8861cd
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-08-30 15:14:43 +0200

    Files generated by PkgTemplates

---
+++ src/Courgette.jl
@@ -0,0 +1,5 @@
+module Courgette
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Courgette
+using Test
+
+@testset "Courgette.jl" begin
+    # Write your own tests here.
+end
